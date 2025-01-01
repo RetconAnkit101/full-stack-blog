c4fc8faf072301dffa4a0a9d1413e18d4fac38c2
Your Name <youremail@example.com>
2025-01-02 00:45:02 +0530

    Pages/Laout/Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Navbar from './Components/Navbar.jsx'
+import { IKImage } from 'imagekitio-react';
+
 const App = () => {
   return (
-    <div className=''>Hello Blogs</div>
+    <div className='px-4 md:px-8 lg:px-16 2xl:px-64'>
+      <Navbar/>
+      {/* <BREADCRUMB/> i.e Home\About etc */}
+      {/* <INTRODUCTION/> */}
+      {/* <FEATURED POSTS/> */}
+      {/* <POST LIST/> */}
+    </div>
+    
   )
 }
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,57 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './routes/LoginPage.jsx';
+import PostListPage from './routes/PostListPage.jsx';
+import Write from './routes/Write.jsx';
+import RegisterPage from './routes/RegisterPage.jsx';
+import HomePage from './routes/HomePage.jsx';
+import MainLayout from './layouts/MainLayout.jsx';
+
+
+const router = createBrowserRouter([
+
+  {
+    element:<MainLayout/>,
+    children: [
+      
+      {
+        path: "/",
+        element: <HomePage/>,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage/>,
+      },
+      {
+        path: "/:slug",
+        element: <PostListPage/>,
+      },
+      {
+        path: "/write",
+        element: <Write/>,
+      },
+      {
+        path: "/login",
+        element: <LoginPage/>,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ]
+  }
+
+  
+  
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
